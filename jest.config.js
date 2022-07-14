68bddee7124404d5a59f8f5869ad36a397b69f8b
Fumiya Sagawa <sagawa.f@tam-tam.co.jp>
2022-07-14 14:51:34 +0900

    fix: JestのmoduleNameMapperを修正

--- jest.config.js
+++ jest.config.js
@@ -10,8 +10,7 @@ const customJestConfig = {
 
   // Handle module aliases (this will be automatically configured for you soon)
   moduleNameMapper: {
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-    '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 }
