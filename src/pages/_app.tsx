85d32d225ea9f2c7d9c135d404285cf92ae9aa7e
Fumiya Sagawa <sagawa.f@tam-tam.co.jp>
2022-10-19 16:59:41 +0900

    update: linterの設定を変更 #6

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
-import '../styles/global.scss'
-
 import type { AppProps } from 'next/app'
+import '../styles/global.scss'
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../../.mocks')
