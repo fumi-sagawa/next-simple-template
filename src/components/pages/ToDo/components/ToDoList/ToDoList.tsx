db87f49d71679063f0fc4da10506280c736cdb0b
Fumiya Sagawa <sagawa.f@tam-tam.co.jp>
2022-10-26 22:18:19 +0900

    chore

--- src/components/pages/ToDo/components/ToDoList/ToDoList.tsx
+++ src/components/pages/ToDo/components/ToDoList/ToDoList.tsx
@@ -1,11 +1,8 @@
 import { Input } from './components/Input'
 import { List } from './components/List'
 import { useToDoList } from './useToDoList'
-import styles from './ToDoList.module.scss'
 
 export const ToDoList = () => {
-  const { todos } = useToDoList()
-
   return (
     <div>
       <h1>ToDo List</h1>
