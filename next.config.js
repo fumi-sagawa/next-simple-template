f450b2256674db803df4f31c9032691c027e6d48
Fumiya Sagawa <sagawa.f@tam-tam.co.jp>
2022-10-27 21:05:01 +0900

    update:next13対応

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ module.exports = {
   images: {
     domains: [],
   },
-  //参考文献
   //https://github.com/vercel/next.js/issues/20271#issuecomment-778838689
-  //https://qiita.com/70ki8suda/items/eb617541379048e87811
   sassOptions: {
     prependData: "@use 'src/styles/' as *;",
   },
-  swcMinify: true,
   env: {
     NEXT_PUBLIC_API_MOCKING: 'enabled',
+  },
+  experimental: {
+    appDir: true,
   },
 }
